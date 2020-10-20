57e03349d59913626ad2d8bd7b291c114d7bcf01
Pokuspock <danniel.carmo@gmail.com>
2020-10-20 11:30:31 -0300

    Mais correções ;-;

---
+++ kitsunevet/Controllers/VeterinarioController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace kitsunevet.Controllers
+{
+    [Controllers]
+    [Route("[Controller]")]
+    public class VeterinarioController
+    {
+        
+    }
+}
