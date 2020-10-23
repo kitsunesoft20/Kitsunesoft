3b88f228a6e27d7480efb3368f48b7e561bd296b
Pokuspock <danniel.carmo@gmail.com>
2020-10-23 04:18:42 -0300

    Correções Home e criação de Componente cabeçalho

---
+++ kitsunevet/src/Components/Cabecalho/index.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import './cabecalho.css';
+import logo from './Kitsunelogo.png';
+
+export default function cabecalho() {
+    return(
+
+        <div className="menu">
+            <div className="submenu">
+                <img src={logo} width="20%" height="20%" alt="NotFound"/>
+                <div className="itensmenu">
+                    <h4>Home</h4>
+                    <div className="sep"> </div>
+                    <h4>Cadastre seu Pet</h4>
+                    <div className="sep"> </div>
+                    <h4> Login </h4>
+                    <div className="sep"> </div>
+                    <h4> Cadastrar </h4>
+                </div>
+            </div>
+        </div>
+
+    );
+
+}
--- kitsunevet/src/Pages/Home/index.js
+++ kitsunevet/src/Pages/Home/index.js
@@ -1,9 +1,95 @@
 
 import React from 'react';
+import './home.css';
+import cabecalho from '../../Components/Cabecalho';
+
+//Import de Imagens abaixo
+
+import exames from './imagens/exames.png';
+import cirurgia from './imagens/cirurgia.png';
+import vacina from './imagens/vacina.png';
+import banhotosa from './imagens/banhotosa.png';
+import hospedagem from './imagens/hospedagem.png';
 
 export default function Home()  {
     return (
-        <h1>Home</h1>
+    <body>
+
+    <div classNameName="inicio">
+
+        <cabecalho/>
+
+        <div className="servicos">
+
+            <div className="titservicos"> O que a Kitsune Vet faz </div>
+
+            <div className="imgservicos">
+
+                <div className="subbox"> 
+                    <img src={hospedagem} width="120px" height="120px" alt="NotFound"/>
+                    <div className="minibox"> <h3>Hospedagem</h3> </div>
+                    <div className="A"><button type="button">Agendar</button></div>
+                </div>
+
+                <div className="subbox"> 
+                    <img src={vacina} width="120px" height="120px" alt="NotFound"/>
+                    <div className="minibox"> <h3>Vacinas</h3> </div>
+                    <div className="A"> <button type="button">Agendar</button> </div>
+                </div>
+
+                <div className="subbox"> 
+                    <img src={cirurgia} width="120px" height="120px" alt="NotFound"/>
+                    <div className="minibox"> <h3>Cirurgia</h3> </div>
+                    <div className="A"> <button type="button">Agendar</button> </div>
+                </div>
+
+                <div className="subbox">
+                    <img src={exames} width="120px" height="120px" alt="NotFound"/>
+                    <div className="minibox"> <h3>Exames</h3> </div>
+                    <div className="A"> <button type="button">Agendar</button> </div>
+                </div>
+
+                <div className="subbox">
+                    <img src={banhotosa} width="120px" height="120px" alt="NotFound"/>
+                    <div className="minibox"> <h3>Banho e Tosa</h3></div>
+                    <div className="A"> <button type="button">Agendar</button> </div>
+                </div>
+
+            </div>
+
+        </div>
+    
+    </div>
+
+    <div className="rodape">
+                
+        <div className="box9">
+
+            <div className="miniboxrodape">
+
+                <p> Serviços</p>
+                <div className="sep"> </div>
+
+                <p> Privacidade</p>
+                <div className="sep"> </div>
+
+                <p> Ajuda</p>
+                <div className="sep"> </div>
+
+                <p> Kitsune</p>
+                  
+            </div>
+
+            <div className="copyright">
+                <p>© 2020 Kitsunesoft BR</p> 
+            </div>
+
+        </div>
+    
+    </div>
+
+    </body>
+
     );
 
 }
